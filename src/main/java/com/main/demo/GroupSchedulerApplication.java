e057a004a4253b28d733d9a55e216f922fa9a9f8
Martin Huynh <martinhuynhp@gmail.com>
2022-06-18 23:41:44 -0700

    Initial commit

---
+++ src/main/java/com/main/demo/GroupSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.main.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupSchedulerApplication.class, args);
+	}
+
+}
